6236f8210648aeafe1083e41c9beb6bdc5060f99
ArthurGuarita <arthur.guarita@sempreceub.com>
2023-03-14 15:35:25 -0300

    solução 1 -- 14/03/2023

---
+++ algoritmo4slide.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1 float64
+	var num2 float64
+	var num3 float64
+	var num4 float64
+	fmt.Print("Digite o primeiro número: ")
+	fmt.Scan(&num1)
+	fmt.Print("Digite o segundo número: ")
+	fmt.Scan(&num2)
+	fmt.Print("Digite o terceiro número: ")
+	fmt.Scan(&num3)
+	fmt.Print("Digite o quarto número: ")
+	fmt.Scan(&num4)
+	var media float64
+	media = (num1 + num2 + num3 + num4) / 4
+	fmt.Print("A média aritmética dos números digitados é: ", media)
+}
---
+++ algoritmo5slide.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Programa feito às 15:33 do dia 14/03/2023 — Caso o valor do dólar esteja diferente, altere.
+
+func main() {
+	var money float64
+	fmt.Print("Digite o valor do dinheiro a ser convertido: ")
+	fmt.Scan(&money)
+	var tipomoney int
+	fmt.Print("Digite 1 para converter dólar para real, ou digite 2 para converter real para dólar: ")
+	fmt.Scan(&tipomoney)
+	if tipomoney == 1 {
+		var valorReal float64
+		valorReal = money * 5.24
+		fmt.Print("O valor convertido é: ", valorReal)
+	}
+	if tipomoney == 2 {
+		var valorDolar float64
+		valorDolar = money / 5.24
+		fmt.Print("O valor convertido é: ", valorDolar)
+	}
+}
