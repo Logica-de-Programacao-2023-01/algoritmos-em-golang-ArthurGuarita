629d7b3d6f8fa29d5ead970d31c4a5044290d34f
Arthur Guaritá Brasil <arthur.guarita@dinfor.uniceub>
2023-03-21 09:18:53 -0300

    nada dms

---
+++ algoritmos - slide/teste.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i <= 10; i++ { //Para o valor inicial de i = 0, enquanto i for menor ou igual a 10, adicione mais um ao i
+		fmt.Println(i)
+	}
+}
